246fd8aed5a2ea688f78a6ab7b3c7b5ed8960a9b
zion bryce <zionbryce2001@gmail.com>
2023-04-12 23:52:12 +0800

    added loading screen

--- lib/screens/auth/pages/loading_dart.dart
+++ lib/screens/auth/pages/loading_dart.dart
@@ -6,10 +6,9 @@ class LoadingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context){
-    return const Scaffold(
-      body : Center(
-        child : CircularProgressIndicator()
-      )
+    return Scaffold(
+      backgroundColor: Color(0xFF263238),
+      body: Center(child: SvgPicture.asset('assets/loading.svg', width: 150, height: 150))
     );
   }
 }
